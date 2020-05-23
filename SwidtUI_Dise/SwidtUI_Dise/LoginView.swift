3d493b399af7858b6b99f8e2fdc69f89b1352c67
Adiel Vasquez <43963033+adielvasquezj@users.noreply.github.com>
2020-05-23 12:38:50 -0400

    TextField and Password

--- SwidtUI_Dise/SwidtUI_Dise/LoginView.swift
+++ SwidtUI_Dise/SwidtUI_Dise/LoginView.swift
@@ -9,9 +9,8 @@
 import SwiftUI
 
 struct LoginView: View {
-    @State  var show = false
-    @State var viewState = CGSize.zero
-    @State var isDragging = false
+    @State var email = ""
+    @State var password = ""
     
     var body: some View {
         ZStack(alignment: .top) {
@@ -21,56 +20,120 @@ struct LoginView: View {
                 .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
                 .edgesIgnoringSafeArea(.bottom)
             
+            CoverView()
+            
             VStack {
-                GeometryReader { geometry in
-                    Text("Learn design & code. \nFrom scratch")
-                        .font(.system(size: geometry.size.width/10, weight: .bold))
-                        .foregroundColor(.white)
-                      
+                HStack {
+                    Image(systemName: "person.crop.circle.fill")
+                        .foregroundColor(Color((#colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1))))
+                        .frame(width: 44, height: 44)
+                        .background(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
+                        .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
+                        .shadow(color: Color.black.opacity(0.15), radius: 5, x: 0, y: 5)
+                        .padding(.leading)
+                    
+                    
+                    TextField("Tu Email".uppercased(), text: $email)
+                        .keyboardType(.emailAddress)
+                        .font(.subheadline)
+                        //  .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(.leading)
+                        .frame(height: 44)
                 }
-                .frame(maxWidth: 375, maxHeight: 100)
-                .padding(.horizontal, 16)
-                .offset(x: viewState.width/15, y: viewState.height/15)
-                
-                
-                Text("By Adiel Jimenez :) 😂")
-                    .font(.subheadline)
-                    .frame(width: 250)
-                  .offset(x: viewState.width/20, y: viewState.height/20)
-                
-                Spacer()
+                Divider().padding(.leading, 80)
+                HStack {
+                    Image(systemName: "lock.fill")
+                        .foregroundColor(Color((#colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1))))
+                        .frame(width: 44, height: 44)
+                        .background(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
+                        .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
+                        .shadow(color: Color.black.opacity(0.15), radius: 5, x: 0, y: 5)
+                        .padding(.leading)
+                    
+                    
+                    SecureField("Contraseña".uppercased(), text: $password)
+                        .keyboardType(.default)
+                        .font(.subheadline)
+                        //  .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(.leading)
+                        .frame(height: 44)
+                }
             }
-            .multilineTextAlignment(.center)
-            .padding(.top, 100)
-            .frame(height: 477)
+            .frame(height: 136)
             .frame(maxWidth: .infinity)
-            .background(
+            .background(BlurView(style: .systemMaterial))
+            .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+            .shadow(color: Color.black.opacity(0.15), radius: 20, x: 0, y: 20)
+            .padding(.horizontal)
+            .offset(y: 460)
+        }
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+        //   .previewDevice("iPad Air 2")
+    }
+}
+
+struct CoverView: View {
+    @State  var show = false
+    @State var viewState = CGSize.zero
+    @State var isDragging = false
+    var body: some View {
+        VStack {
+            GeometryReader { geometry in
+                Text("Learn design & code. \nFrom scratch")
+                    .font(.system(size: geometry.size.width/10, weight: .bold))
+                    .foregroundColor(.white)
                 
-                ZStack {
-                    Image(uiImage: #imageLiteral(resourceName: "Blob"))
-                        .offset(x: -150, y: -200)
-                        .rotationEffect(Angle(degrees: show ? 360+90 : 90))
-                        .blendMode(.plusDarker)
-                        .animation(Animation.linear(duration: 120).repeatForever(autoreverses: false))
-                        .onAppear { self.show = true }
-                    Image(uiImage: #imageLiteral(resourceName: "Blob"))
-                        .offset(x: -200, y: -250)
-                        .rotationEffect(Angle(degrees: show ? 360 : 0), anchor: .leading)
-                        .blendMode(.overlay)
-                        .animation(Animation.linear(duration: 120)
-                            .repeatForever(autoreverses: false))
-            })
-                .background(ZStack {
-                    Image(uiImage: #imageLiteral(resourceName: "Card2"))
-                      .offset(x: viewState.width/25, y: viewState.height/26)
-                }, alignment: .bottom)
-                .background(Color(#colorLiteral(red: 0.4078431373, green: 0.4705882353, blue: 0.9725490196, alpha: 1)))
-                .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
-                 .scaleEffect(isDragging ? 0.9 : 1)
-                .animation(.timingCurve(0.2, 0.8, 0.2, 1, duration: 0.8))
-                .rotation3DEffect(Angle(degrees: 5), axis: (x: viewState.width, y: viewState.height, z: 0))
-               
-                
+            }
+            .frame(maxWidth: 375, maxHeight: 100)
+            .padding(.horizontal, 16)
+            .offset(x: viewState.width/15, y: viewState.height/15)
+            
+            
+            Text("By Adiel Jimenez :) 😂")
+                .font(.subheadline)
+                .frame(width: 250)
+                .offset(x: viewState.width/20, y: viewState.height/20)
+            
+            Spacer()
+        }
+        .multilineTextAlignment(.center)
+        .padding(.top, 100)
+        .frame(height: 477)
+        .frame(maxWidth: .infinity)
+        .background(
+            
+            ZStack {
+                Image(uiImage: #imageLiteral(resourceName: "Blob"))
+                    .offset(x: -150, y: -200)
+                    .rotationEffect(Angle(degrees: show ? 360+90 : 90))
+                    .blendMode(.plusDarker)
+                    //  .animation(Animation.linear(duration: 120).repeatForever(autoreverses: false))
+                    .animation(nil)
+                    .onAppear { self.show = true }
+                Image(uiImage: #imageLiteral(resourceName: "Blob"))
+                    .offset(x: -200, y: -250)
+                    .rotationEffect(Angle(degrees: show ? 360 : 0), anchor: .leading)
+                    .blendMode(.overlay)
+                    // .animation(Animation.linear(duration: 120)
+                    //.repeatForever(autoreverses: false))
+                    .animation(nil)
+        })
+            .background(ZStack {
+                Image(uiImage: #imageLiteral(resourceName: "Card2"))
+                    .offset(x: viewState.width/25, y: viewState.height/26)
+            }, alignment: .bottom)
+            .background(Color(#colorLiteral(red: 0.4078431373, green: 0.4705882353, blue: 0.9725490196, alpha: 1)))
+            .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+            .scaleEffect(isDragging ? 0.9 : 1)
+            .animation(.timingCurve(0.2, 0.8, 0.2, 1, duration: 0.8))
+            .rotation3DEffect(Angle(degrees: 5), axis: (x: viewState.width, y: viewState.height, z: 0))
+            
+            
             .gesture(
                 DragGesture().onChanged { value in
                     self.viewState = value.translation
@@ -80,15 +143,6 @@ struct LoginView: View {
                     self.viewState = .zero
                     self.isDragging = false
                 }
-            )
-        }
-        
-    }
-}
-
-struct LoginView_Previews: PreviewProvider {
-    static var previews: some View {
-        LoginView()
-        //   .previewDevice("iPad Air 2")
+        )
     }
 }
