6839b58b7feed93aff3948fc93baba0e199aacc0
Adiel Vasquez <43963033+adielvasquezj@users.noreply.github.com>
2020-06-02 14:27:34 -0400

    Cómo oscurecer una imagen con capas adicionales y Final

--- First_App/02_Images/02_Images/ContentView.swift
+++ First_App/02_Images/02_Images/ContentView.swift
@@ -7,7 +7,7 @@
 //
 
 import SwiftUI
-
+//Cómo oscurecer una imagen con capas adicionales
 struct ContentView: View {
     var body: some View {
         GeometryReader { bounds in
@@ -26,15 +26,22 @@ struct ContentView: View {
                      .foregroundColor(Color(#colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)))
                      .opacity(0.5)*/
                     
-                    GeometryReader { bounds in
-                        Text("Seguridad en el manejo de archivos Tu privacidad está garantizada. Nadie tiene acceso a tus archivos JPG y PDF y se eliminarán de manera permanente de nuestros servidores cuando haya pasado una hora.")
-                            .font(.system(size: 22, weight: .heavy, design: .rounded))
-                            .padding()
-                            .background(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)).opacity(0.5))
-                            .cornerRadius(10)
-                            .padding()
-                    } , alignment: .center
-                    
+                    /* GeometryReader { bounds in
+                     Text("Seguridad en el manejo de archivos Tu privacidad está garantizada. Nadie tiene acceso a tus archivos JPG y PDF y se eliminarán de manera permanente de nuestros servidores cuando haya pasado una hora.")
+                     .font(.system(size: 22, weight: .heavy, design: .rounded))
+                     .padding()
+                     .background(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)).opacity(0.5))
+                     .cornerRadius(10)
+                     .padding()
+                     } , alignment: .center
+                     
+                     */
+                    Capsule()
+                        .foregroundColor(Color(#colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)))
+                        .opacity(0.3)
+                        .overlay(Text("New York"))
+                        .font(.title)
+                        .foregroundColor(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
                     
             )
         }
