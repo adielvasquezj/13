e7cb8a5ebd54aad248edfe9b6f69f06343f3149a
Adiel Vasquez <43963033+adielvasquezj@users.noreply.github.com>
2020-06-02 11:40:06 -0400

    Completada :/

--- First_App/01_First_App/01_First_App/ContentView.swift
+++ First_App/01_First_App/01_First_App/ContentView.swift
@@ -10,11 +10,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
-            .font(.system(.largeTitle, design: .rounded))
-        .foregroundColor(Color(#colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)))
-           
-            
+        Text("""
+ Working with a team? GitHub is built for collaboration. Set up an organization to improve the way your team works together, and get access to more features.
+""")
+            .fontWeight(.regular)
+            .font(.custom("Gill Sans", size: 25))
+            .foregroundColor(Color(#colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)))
+            //Fondo costumizado
+            .multilineTextAlignment(.center)
+            .lineLimit(nil)
+            .truncationMode(.middle)
+            .lineSpacing(8)
+            .padding()
+            //.rotationEffect(.init(degrees: 15), anchor: UnitPoint(x: 0, y: 0))
+            .rotation3DEffect(.init(degrees: 50), axis: (x: 1, y: 0, z: 0))
+            .shadow(color: Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)), radius: 1, x: 0, y: 5)
     }
 }
 
