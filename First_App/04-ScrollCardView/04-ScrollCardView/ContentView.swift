69839d5f2d626b682582585cd7f0fdcc9b135e0a
Adiel Vasquez <43963033+adielvasquezj@users.noreply.github.com>
2020-06-03 19:03:11 -0400

    CardView Parametrizada

--- First_App/04-ScrollCardView/04-ScrollCardView/ContentView.swift
+++ First_App/04-ScrollCardView/04-ScrollCardView/ContentView.swift
@@ -9,37 +9,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    var imageName: String
+    var planetName: String
+    var Subline: String
+    var cross: String
+    
     var body: some View {
         VStack {
-            Image("marte")
+            Image(imageName)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
-            
-            VStack(alignment: .leading) {
-                Text("Marte")
-                    .font(.title)
-                    .fontWeight(.bold)
-                
-                Text("Marte un planeta rocozo")
-                    .fontWeight(.semibold)
-                    .foregroundColor(.secondary)
-              //  .strikethrough()
-                
-                HStack {
-                    Text("Marte no es un planeta gaseoso")
-                    .strikethrough()
-                        .font(.caption)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text(planetName)
+                        .font(.title)
+                        .fontWeight(.bold)
+                    
+                    Text(Subline.uppercased())
+                        .fontWeight(.semibold)
+                        .foregroundColor(.secondary)
+                    //  .strikethrough()
+                    
+                    HStack {
+                        Text(Subline)
+                            .strikethrough()
+                            .font(.caption)
+                    }
+                    
                 }
-                 
+                .layoutPriority(100)
+                Spacer()
             }
-      
+            .padding()
         }
+        .cornerRadius(12)
+        .overlay(RoundedRectangle(cornerRadius: 12).stroke(Color(#colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)).opacity(0.2), lineWidth: 1)
+            
+        )
+            
+            .padding([.top, .horizontal])
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(imageName: "marte", planetName: "Marte", Subline: "Marte es un planeta rocoso", cross: "Marte no es un planeta gaseoso")
     }
 }
 
