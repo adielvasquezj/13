0a81f47b74a73c55297c071c2d58947459a54053
Adiel Vasquez <43963033+adielvasquezj@users.noreply.github.com>
2020-06-03 21:35:03 -0400

    Revert "Cbecera peronalizada"
    This reverts commit a3854bf6c6f5ec08334e14401f1814c93c5d81e5.

--- First_App/04-ScrollCardView/04-ScrollCardView/CardView.swift
+++ First_App/04-ScrollCardView/04-ScrollCardView/CardView.swift
@@ -12,35 +12,10 @@ struct CardView: View {
     var body: some View {
         GeometryReader { bounds in
             ScrollView {
-                
-                VStack {
-                    Image("saturno")
-                        .resizable()
-                        
-                     .frame(maxWidth: 50, maxHeight: 50)
-                         .cornerRadius(12)
-                  .padding(50)
-                        .aspectRatio(contentMode: .fit)
-                       // .padding(-10)
-                       
-                  
-                        
-
-                    Text("Aprende mas sobre los")
-                        .fontWeight(.bold )
-                    .offset(x: 0, y: -30)
-                    Text("Planetas")
-                        .fontWeight(.light)
-                      .offset(x: 0, y: -20)
-                }
-          
-                .foregroundColor(.primary)
-              
-                
             ContentView(imageName: "marte", planetName: "Marte", Subline: "Marte es un planeta rocoso", cross: "Marte no es  gaseoso")
             ContentView(imageName: "luna", planetName: "La luna", Subline: "La luna es un satelite natural de la tierra", cross: "La luna no es un planeta")
             ContentView(imageName: "saturno", planetName: "Saturno", Subline: "Saturno es un planeta gaseoso", cross: "Saturno no es un planeta rocoso")
-            ContentView(imageName: "vetus", planetName: "Venus", Subline: "Venus es un planeta rocoso", cross: "Venus no es un planeta gaseoso")
+            ContentView(imageName: "venus", planetName: "Venus", Subline: "Venus es un planeta rocoso", cross: "Venus no es un planeta gaseoso")
             }
         }
     }
