25e108d37483903266f2c6460f6369b2ab2adaae
Adiel Vasquez <43963033+adielvasquezj@users.noreply.github.com>
2020-06-04 13:05:24 -0400

    Un botón con texto eimagen

--- First_App/05_ Buttons/05_ Buttons/ContentView.swift
+++ First_App/05_ Buttons/05_ Buttons/ContentView.swift
@@ -11,26 +11,45 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        Button(action: {
-            //Qué debe hacer el botón
-            print("Hello world pulsado")
-        }) {
-            //Cómo debe verse el botón
-            Text("Hello world")
-               // .padding()
-                 .font(.largeTitle)
-            .padding()
-                .background(Color(#colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)))
+        VStack {
+            Button(action: {
+                //Qué debe hacer el botón
+                print("Hello world pulsado")
+            }) {
+                //Cómo debe verse el botón
+                Text("Hello world")
+                   // .padding()
+                     .font(.largeTitle)
+                .padding()
+                    .background(Color(#colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)))
+                    .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                   // .font(.largeTitle)
+                .cornerRadius(50)
+                   .padding(12)
+                .overlay(
+                RoundedRectangle(cornerRadius: 50)
+                 .stroke(Color(#colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)), lineWidth: 4)
+                )
+                    //.border(Color(#colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)), width: 6)
+                    
+            }
+            
+            
+            Button(action: {
+                print("Botón con icono pulsado")
+            }) {
+                HStack {
+               Image(systemName: "trash")
+                    Text("Eliminar")
+                }
+                .padding()
+                .background(Color(#colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1)))
+               // .clipShape(Circle())
+                .font(.largeTitle)
                 .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
-               // .font(.largeTitle)
-            .cornerRadius(50)
-               .padding(12)
-            .overlay(
-            RoundedRectangle(cornerRadius: 50)
-             .stroke(Color(#colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)), lineWidth: 4)
-            )
-                //.border(Color(#colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)), width: 6)
-                
+                .cornerRadius(40)
+            }
+            
         }
         
     }
