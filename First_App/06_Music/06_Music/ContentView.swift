e8a5fff718f69dee6a68b5553187450abc33cb2f
Adiel Vasquez <43963033+adielvasquezj@users.noreply.github.com>
2020-06-04 20:00:57 -0400

    Cócmo cambiar dinamicamente la apariencia de un botón

---
+++ First_App/06_Music/06_Music/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  06_Music
+//
+//  Created by roott on 6/4/20.
+//  Copyright © 2020 roott. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    private var isPlaying = false
+    
+    var body: some View {
+        
+        Button(action: {
+            print("Boton pulsado")
+        }) {
+            Image(systemName: isPlaying ? "stop.circle.fill" : "play.circle.fill")
+            .font(.system(size: 100))
+            .foregroundColor(isPlaying ? .red : .blue)
+            
+        
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
