3d8ceab321151c17e269b37737821fc379b9e7b6
Adiel Vasquez <43963033+adielvasquezj@users.noreply.github.com>
2020-06-06 12:26:23 -0400

    Fill Stroke

--- First_App/07_Draw/07_Draw/ContentView.swift
+++ First_App/07_Draw/07_Draw/ContentView.swift
@@ -11,19 +11,20 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
+            /*
             Path() { path in
                 
-                 path.move(to: CGPoint(x: 60, y: 130))
-                 path.addLine(to: CGPoint(x: 130, y: 0))
-                 path.addLine(to: CGPoint(x: 200, y: 130))
+                path.move(to: CGPoint(x: 60, y: 130))
+                path.addLine(to: CGPoint(x: 130, y: 0))
+                path.addLine(to: CGPoint(x: 200, y: 130))
                 
                 
-            }
+            }*/
             
             Path() { path in
                 path.move(to: CGPoint(x: 30, y: 130))
                 path.addLine(to: CGPoint(x: 60, y: 130))
-                path.addQuadCurve(to: CGPoint(x: 200, y: 130), control: CGPoint(x: 130, y: 0))
+                path.addQuadCurve(to: CGPoint(x: 200, y: 130), control: CGPoint(x: 130, y: 60))
                 path.addLine(to: CGPoint(x: 230, y: 130))
                 path.addLine(to: CGPoint(x: 230, y: 250))
                 path.addLine(to: CGPoint(x: 30, y: 250))
@@ -32,6 +33,20 @@ struct ContentView: View {
             }
             .fill(Color(#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)))
             // .stroke(Color(#colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)), lineWidth: 10)
+            
+            Path() { path in
+                path.move(to: CGPoint(x: 30, y: 130))
+                path.addLine(to: CGPoint(x: 60, y: 130))
+                path.addQuadCurve(to: CGPoint(x: 200, y: 130), control: CGPoint(x: 130, y: 60))
+                path.addLine(to: CGPoint(x: 230, y: 130))
+                path.addLine(to: CGPoint(x: 230, y: 250))
+                path.addLine(to: CGPoint(x: 30, y: 250))
+             path.closeSubpath()
+                
+            }
+            //.fill(Color(#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)))
+             .stroke(Color(#colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)), lineWidth: 10)
+           
             
         }
     }
